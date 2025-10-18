6f6f96c199318f6a6c34a627634f2441228dc0cb
Andres Morey <andresmarcel@gmail.com>
2025-10-18 08:31:16 +0300

    ref: use pre-prepareed vendored source for build step (#3)
    ref: Use pre-prepared .orig.tar.xz tarball prepared with pre-built
         frontend assets and vendored go module
    ref: Remove frontend/backend build strategy with one buildGoModule
    ref: Replace custom buildPhase with standard buildGoModule options
    
    Signed-off-by: Andres Morey <andresmarcel@gmail.com>

--- default.nix
+++ default.nix
@@ -11,95 +11,46 @@
   ) { },
 }:
 
-let
+pkgs.buildGoModule rec {
   pname = "kubetail";
   version = "0.9.0";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "kubetail-org";
-    repo = "kubetail";
-    tag = "cli/v${version}";
-    sha256 = "sha256-n5kHK/cJcDfCy/zQBtHPAxVCnm6RKvHwB5P1o3wthuM=";
+  src = pkgs.fetchurl {
+    url = "https://github.com/kubetail-org/kubetail/releases/download/cli%2Fv${version}/kubetail_${version}.orig.tar.xz";
+    hash = "sha256-nb3XL8tyM0B97iDDxHaanCwl1PWOEsP8aFPzL86SWK0=";
   };
 
-  frontend = pkgs.stdenv.mkDerivation {
-    inherit version src;
-    pname = "${pname}-frontend";
+  nativeBuildInputs = with pkgs; [
+    installShellFiles
+  ];
 
-    nativeBuildInputs = [
-      pkgs.nodejs
-      pkgs.nodePackages.pnpm
-      pkgs.pnpm.configHook
-    ];
+  modRoot = "modules/cli";
+  subPackages = [ "." ];
 
-    pnpmRoot = "dashboard-ui";
+  # Source is already vendored so we can ignore the vendor hash
+  vendorHash = null;
 
-    pnpmDeps = pkgs.pnpm.fetchDeps {
-      inherit pname version;
-      src = "${src}/dashboard-ui";
-      hash = "sha256-Ph06qKzdle4nFU7PQhrGlxOeP13EqtG43tVd55GkTvg=";
-      fetcherVersion = 2;
-    };
+  env.GOWORK = "off";
 
-    buildPhase = ''
-      cd dashboard-ui
-      pnpm build
-      cd ../
-    '';
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/kubetail-org/kubetail/modules/cli/cmd.version=${version}"
+  ];
 
-    installPhase = ''
-      cp -r dashboard-ui/dist $out
-    '';
+  doCheck = false;
+
+  postInstall = ''
+    mv $out/bin/{cli,${pname}}
+    installShellCompletion --cmd ${pname}         \
+      --bash <($out/bin/${pname} completion bash) \
+      --fish <($out/bin/${pname} completion fish) \
+      --zsh  <($out/bin/${pname} completion zsh)
+  '';
+
+  meta = {
+    description = "Real-time logging dashboard for Kubernetes";
+    homepage = "https://github.com/kubetail-org/kubetail";
+    mainProgram = pname;
   };
-
-  backend = pkgs.buildGoModule {
-    inherit pname version src;
-
-    nativeBuildInputs = with pkgs; [
-      installShellFiles
-    ];
-
-    modRoot = "modules/cli";
-    subPackages = [ "." ];
-
-    vendorHash = "sha256-lv18257J2txfCWlusSCUQTr/CBxbnwqOSePtGUpRqBE=";
-
-    env.GOWORK = "off";
-
-    ldflags = [
-      "-s"
-      "-w"
-      "-X github.com/kubetail-org/kubetail/modules/cli/cmd.version=${version}"
-    ];
-
-    doCheck = false;
-
-    buildPhase = ''
-      runHook preBuild
-
-      chmod -R +w vendor/github.com/kubetail-org/kubetail/modules/dashboard
-      rm -rf vendor/github.com/kubetail-org/kubetail/modules/dashboard/website
-      cp -r ${frontend} vendor/github.com/kubetail-org/kubetail/modules/dashboard/website
-
-      go build -mod="vendor" -ldflags="$ldflags" -o "$GOPATH/bin/" main.go
-
-      runHook postBuild
-    '';
-
-    postInstall = ''
-      mv $out/bin/{main,${pname}}
-
-      installShellCompletion --cmd ${pname}         \
-        --bash <($out/bin/${pname} completion bash) \
-        --fish <($out/bin/${pname} completion fish) \
-        --zsh  <($out/bin/${pname} completion zsh)
-    '';
-
-    meta = {
-      description = "Real-time logging dashboard for Kubernetes";
-      homepage = "https://github.com/kubetail-org/kubetail";
-      mainProgram = pname;
-    };
-  };
-in
-backend
+}
